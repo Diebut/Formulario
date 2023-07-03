b739157e9f11077d35396ad16399289c9e65bafa
Diebut <guevaradiego_2011@hotmail.com>
2023-07-02 21:43:32 -0300

    edito la validacion

--- script.js
+++ script.js
@@ -6,14 +6,21 @@ buttonEnviar.addEventListener('click', function () {
 
     let inputNombre = document.getElementById('input-nombre');
     let inputCorreo = document.getElementById('input-correo');
+    let pMensaje = document.getElementById('p-mensaje-error');
+    let pMensajeConfirmacion = document.getElementById('p-mensaje-confirmacion');
 
     if (inputNombre.value == "") {
-        alert("Error: El Nombre esta vacio.")
+        //alert("Error: El Nombre esta vacio.")
+        pMensaje.innerHTML = "Error: El Nombre esta vacio."
     }
 
-    if (inputCorreo.value == "") {
-        alert("Error: El Correo esta vacio.")
+    else if (inputCorreo.value == "") {
+        //alert("Error: El Correo esta vacio.")
+        pMensaje.innerHTML = "Error: El Correo esta vacio."
+    }else{
+        pMensajeConfirmacion.innerHTML = "los datos se enviaron correctamente";
     }
+   
 
 });
 
